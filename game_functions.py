5321c2fd68026231101fe5a300527e4ebf7c16bb
wisteriamealybug <memh201@exeter.ac.uk>
2026-01-16 12:11:39 +0000

    binary search

--- game_functions.py
+++ game_functions.py
@@ -2,8 +2,8 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
-    return x
+    i = len(poss_values) // 2 
+    return poss_values[i]
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
